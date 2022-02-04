7f0ba3a62a1d816268a1700abb7b1eee6aebd52a
nimachapi1994 <nimachapi1994@gmail.com>
2022-02-04 06:30:51 -0800

    create view components folder and create project view componenet and patrial view and show in index page

---
+++ DevCard/DevCard_Mvc/ViewCompenents/ProjectViewComponents.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DevCard_Mvc.ViewCompenents
+{
+    [ViewComponent(Name = "Project")]
+    public class ProjectViewComponents:ViewComponent
+    {
+      
+        public IViewComponentResult Invoke()
+        {
+            return View("Project");
+        }
+    }
+}
