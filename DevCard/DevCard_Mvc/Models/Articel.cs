cbb5d50520c4eeae55e1cad9dd2a30222d860a06
nimachapi1994 <nimachapi1994@gmail.com>
2022-02-04 05:23:41 -0800

    create atricle and project view model move article and project html sections to partial view and render to html.cshtml

---
+++ DevCard/DevCard_Mvc/Models/Articel.cs
@@ -0,0 +1,9 @@
+﻿namespace DevCard_Mvc.Models
+{
+    public class Articel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DevCard/DevCard_Mvc/Models/IndexModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DevCard_Mvc.Models
+{
+    public class IndexModel
+    {
+        public List<Project> projects { get; set; }
+        public List<Articel> articels { get; set; }
+
+    }
+}
